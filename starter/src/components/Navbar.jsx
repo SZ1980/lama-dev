58ce37af64a59a60d1513982c4839526130856c5
Zoran Simonovic <simonovic1980@gmail.com>
2023-02-06 18:18:21 +0100

    download material ui

--- starter/src/components/Navbar.jsx
+++ starter/src/components/Navbar.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input></Input>
+          </SearchContainer>
+        </Left>
+        <Center>center</Center>
+        <Right>right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
