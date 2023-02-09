368b8574b91c45140a3e878483ea537a7e7640cb
Zoran Simonovic <simonovic1980@gmail.com>
2023-02-09 14:58:30 +0100

    finishing Announcement.jsx file

--- starter/src/components/Navbar.jsx
+++ starter/src/components/Navbar.jsx
@@ -1,5 +1,8 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
+//import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
@@ -40,11 +43,21 @@ const Center = styled.div`
   text-align: center;
 `;
 
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Navbar = () => {
@@ -54,11 +67,22 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input></Input>
+            <Input placeholder="Search" />
+            <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
-        <Center>center</Center>
-        <Right>right</Right>
+        <Center>
+          <Logo>NINA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
